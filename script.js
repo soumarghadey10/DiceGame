a8d6919c61257288c3e8207725e89a2f1afe649e
soumarghadey10 <124905203+soumarghadey10@users.noreply.github.com>
2023-06-18 23:06:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource =  randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🎉Player 1 Wins!";
+  document.querySelector("h3").innerHTML="Refresh again to paly 🎲";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🎉Player 2 Wins!";
+  document.querySelector("h3").innerHTML="Refresh again to paly 🎲";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+  document.querySelector("h3").innerHTML="Refresh again to paly 🎲";
+}
